8d61ebe3adcce1dc0106d2d79b4c654eab44686a
cassiano yasumitsu <cassiano@usuarioincomum.com>
2022-02-20 20:05:54 +0900

    secret key created

--- src/config/config.go
+++ src/config/config.go
@@ -12,6 +12,7 @@ import (
 var (
 	StringConnectionDB = ""
 	APIDoor            = 0
+	SecretKey          []byte
 )
 
 // Inicializa variaveis de ambiente
@@ -32,4 +33,6 @@ func Load() {
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
 	)
+
+	SecretKey = []byte(os.Getenv("SECRET_KEY"))
 }
