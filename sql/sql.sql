559a19cd717806050a81da058b51e9fbd25a16f5
cassiano yasumitsu <cassiano@usuarioincomum.com>
2022-03-24 16:49:58 +0900

    add posts table

--- sql/sql.sql
+++ sql/sql.sql
@@ -4,8 +4,9 @@
 CREATE DATABASE IF NOT EXIST devbook;
 USE devbook;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS followers;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users(
   id int auto_increment primary key,
@@ -26,4 +27,18 @@ CREATE TABLE followers(
   ON DELETE CASCADE,
 
   primary key (user_id, follower_id)
+) ENGINE=INNODB;
+
+
+CREATE TABLE posts(
+  id int auto_increment primary key,
+  title varchar(50) not null,
+  content text not null,
+
+  author_id int not null,
+  FOREIGN KEY (author_id) REFERENCES users(id)
+  ON DELETE CASCADE,
+
+  likes int default 0,
+  created_at timestamp default current_timestamp()
 ) ENGINE=INNODB;
